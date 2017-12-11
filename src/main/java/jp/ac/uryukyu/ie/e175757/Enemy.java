d3807ef362ad4b8d901d5ad6efdec89fa58fa967
Kosuke Hanashiro <e175757@ie.u-ryukyu.ac.jp>
2017-12-11 20:43:39 +0900

    ステップ3

--- src/main/java/jp/ac/uryukyu/ie/e175757/Enemy.java
+++ src/main/java/jp/ac/uryukyu/ie/e175757/Enemy.java
@@ -20,5 +20,27 @@ public class Enemy extends LivingThing {
             System.out.printf("モンスター%sは倒れた。\n", getName());
         }
     }
+    @Override
+    public void attack(LivingThing opponent){
+        int critical=(int)(Math.random()*10);
+        int damage=(int) (Math.random() * getAttack());
+        if(isDead()!=true) {
+            if(critical<=3){
+                if(damage!=0) {
+                    System.out.printf("%sの攻撃！会心の一撃！！%sに%dのダメージを与えた！！\n", getName(), opponent.getName(), damage*2);
+                    opponent.wounded(damage*2);
+                }else{
+                    System.out.printf("%sの攻撃！,,,だが、%sは攻撃を回避した！\n", getName(), opponent.getName());
+                }
+            }else{
+                if(damage!=0){
+                    System.out.printf("%sの攻撃！%sに%dのダメージを与えた！！\n", getName(), opponent.getName(), damage);
+                    opponent.wounded(damage);
+                }else {
+                    System.out.printf("%sの攻撃！,,,だが、%sは攻撃を回避した！\n", getName(), opponent.getName(), damage);
+                }
+            }
+        }
+    }
 
 }
--- src/main/java/jp/ac/uryukyu/ie/e175757/Hero.java
+++ src/main/java/jp/ac/uryukyu/ie/e175757/Hero.java
@@ -13,7 +13,6 @@ public class Hero extends LivingThing{
 
     public Hero (String name, int maximumHP, int attack) {
         super(name,maximumHP,attack);
-
     }
 
     @Override
@@ -24,5 +23,25 @@ public class Hero extends LivingThing{
             System.out.printf("勇者%sは道半ばで力尽きてしまった。\n", getName());
         }
     }
+
+    public void attack(LivingThing opponent){
+        int critical=(int)(Math.random()*10);
+        int damage=(int) (Math.random() * getAttack());
+
+        if(isDead()!=true) {
+            if(damage==0){
+                System.out.printf("%sの攻撃！,,,だが、%sは攻撃を回避した！\n", getName(), opponent.getName(), damage);
+
+            }else if(critical<4) {
+                System.out.printf("%sの攻撃！会心の一撃！！%sに%dのダメージを与えた！！\n", getName(), opponent.getName(), damage*2);
+                opponent.wounded(damage*2);
+
+            }else {
+                System.out.printf("%sの攻撃！%sに%dのダメージを与えた！！\n", getName(), opponent.getName(), damage);
+                opponent.wounded(damage);
+                 }
+            }
+        }
+
 }
 
